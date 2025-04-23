b171e0376e5e0810cc26729f27593ca2bd202af4
Matt <mattfant@DESKTOP-EOTD14K.>
2025-04-23 00:54:15 -0400

    word counter added

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+from stats import get_word_count
+
+def get_book_text(filepath):
+    with open(f"{filepath}") as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    book = get_book_text("books/frankenstein.txt")
+    word_count = get_word_count()
+    print(f"{word_count} words found in the document")
+    
+main()
